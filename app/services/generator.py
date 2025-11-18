04b284089a802bef5d15e491c0ec210ca6d1ef64
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2025-11-18 23:01:04 +0100

    feat: finish implement queue and job

--- app/services/generator.py
+++ app/services/generator.py
@@ -1,26 +1,7 @@
-
-
-
 def generate_script(config: dict) -> str:
-    interface = ''
-    script = ''
-    for key, value in config.items():
-        if key == "hostname":
-            script += f"hostname {value}\n"
-        elif key == "interfaces":
-            for value in config[key]:
-                interface += f"interface {value['name']}\n"
-                interface += f" ip address {value['ip']} {value['mask']}\n"
-                interface += " no shutdown\n"
-            script += interface
+    script = f"hostname {config.get('hostname', '')}\n"
+    for iface in config.get("interfaces", []):
+        script += f"interface {iface['name']}\n"
+        script += f" ip address {iface['ip']} {iface['mask']}\n"
+        script += " no shutdown\n"
     return script
-
-
-config = {
-    "hostname": "R1",
-    "interfaces": [
-        {"name": "Gig1", "ip": "10.0.0.1", "mask": "255.255.255.0"}
-    ]
-}
-
-print(generate_script(config))
